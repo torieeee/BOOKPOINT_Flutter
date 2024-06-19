f9a3a4f71d2dff235dd71a64bc94239a1c4271f1
torieeee <victoria.mwaura@strathmore.edu>
2024-06-19 10:30:21 +0300

    form_container_widget

---
+++ lib/shared/theme/widgets/form_container_widget.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class FormContainerWidget extends StatefulWidget {
+  const FormContainerWidget({super.key});
+
+  @override
+  State<FormContainerWidget> createState() => _FormContainerWidgetState();
+}
+
+class _FormContainerWidgetState extends State<FormContainerWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
