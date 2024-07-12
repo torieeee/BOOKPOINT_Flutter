582866fedd2549698b2ee16487b9ba3c44e66a10
Sean Ratemo <sean.ratemo@gmail.com>
2024-07-12 23:51:21 +0300

    Clean Versioning

--- lib/models/Prescribe.dart
+++ lib/models/Prescribe.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:provider/provider.dart';
+
 
 class Prescribe extends ChangeNotifier {
   String? prescriptionId;
