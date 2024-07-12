35f6cd6a4eed0e2bb45a5c2bec56a601438c69a6
Sean Ratemo <sean.ratemo@gmail.com>
2024-07-13 00:10:07 +0300

    Making Icon Visible

--- lib/doctor_section/view_profile.dart
+++ lib/doctor_section/view_profile.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
-import '../utils/config.dart';
 
 class ViewDoctorProfile extends StatelessWidget {
   final String userId;
