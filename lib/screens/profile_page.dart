882f9ce1971b4fe221a9998e5f67b1a300962029
Sean Ratemo <sean.ratemo@gmail.com>
2024-07-12 21:36:13 +0300

    Border rectification for profile page

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../utils/config.dart';
 import 'package:intl/intl.dart';
 
 class ProfilePage extends StatefulWidget {
