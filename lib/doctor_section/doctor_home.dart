006fc63eb12296f12a71840168a3ebdbea85c7c2
Sean Ratemo <sean.ratemo@gmail.com>
2024-07-13 00:31:09 +0300

    Declatering of the home page

--- lib/doctor_section/doctor_home.dart
+++ lib/doctor_section/doctor_home.dart
@@ -1,157 +1,12 @@
 import 'dart:ui';
-
-//import 'package:book_point/doctor_section/requests.dart';
 import 'package:book_point/shared/theme/widgets/titles/section_title.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-//import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import '../models/auth_model.dart';
 import '../shared/theme/widgets/cards/requests_card.dart';
-//import '../utils/config.dart';
-//import '../components/doctor_card.dart';
-//import 'requests.dart';
 
-/*class DoctorHome extends StatelessWidget {
-  const DoctorHome({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const DoctorView();
-  }
-}
-
-class DoctorView extends StatelessWidget {
-  const DoctorView({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    
-    final authModel = Provider.of<AuthModel>(context);
-    final textTheme = Theme.of(context).textTheme;
-    final colorScheme = Theme.of(context).colorScheme;
-
-    return Scaffold(
-      appBar: AppBar(
-        toolbarHeight: 144,
-        backgroundColor: const Color(0xFFFFFFFF),
-        title: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const SizedBox(width: 8.0),
-            Text(
-              'Welcome',
-              style: GoogleFonts.sora(
-                textStyle: textTheme.bodyMedium!.copyWith(
-                  color: colorScheme.primary,
-                ),
-              ),
-            ),
-            Config.spaceSmall,
-            FutureBuilder<DocumentSnapshot>(
-              future: FirebaseFirestore.instance
-                  .collection('Users')
-                  .doc(authModel.firebaseUser?.uid)
-                  .get(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return const CircularProgressIndicator();
-                } else if (snapshot.hasError || !snapshot.hasData || !snapshot.data!.exists) {
-                  return Text(
-                    'Daktari',
-                    style: GoogleFonts.sora(
-                      textStyle: textTheme.bodyMedium!.copyWith(
-                        color: colorScheme.primary,
-                      ),
-                    ),
-                  );
-                } else {
-                  final userData = snapshot.data!.data() as Map<String, dynamic>;
-                  return Text(
-                    userData['username'] ?? 'Daktari',
-                    style: GoogleFonts.sora(
-                      textStyle: textTheme.bodyMedium!.copyWith(
-                        color: colorScheme.primary,
-                      ),
-                    ),
-                  );
-                }
-              },
-            ),
-            const SizedBox(height: 4.0),
-            Row(
-              children: [
-                Icon(
-                  Icons.location_on,
-                  color: colorScheme.secondary,
-                ),
-                const SizedBox(width: 4.0),
-                Text(
-                  'Nairobi, Kenya',
-                  style: GoogleFonts.sora(
-                    textStyle: textTheme.bodySmall,
-                  ),
-                ),
-                const SizedBox(height: 4.0),
-                Icon(
-                  Icons.expand_more,
-                  color: colorScheme.secondary,
-                ),
-              ],
-            ),
-          ],
-        ),
-       /* actions: [
-          IconButton(
-            icon: const Icon(FontAwesomeIcons.signOutAlt),
-            onPressed: () {
-              authModel.logout();
-            },
-          ),
-        ],*/
-      ),
-      body: StreamBuilder<DocumentSnapshot>(
-        stream: FirebaseFirestore.instance
-            .collection('doctors')
-            .doc(authModel.firebaseUser?.uid)
-            .snapshots(),
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || !snapshot.data!.exists) {
-            return const Center(child: Text('No data found'));
-          }
-
-          final doctorData = snapshot.data!.data() as Map<String, dynamic>;
-
-          return Column(
-            children: [
-              Config.spaceMedium,
-              DoctorCard(doctor: doctorData, isFav: false,),
-              Config.spaceMedium,
-              Expanded(
-                child: ListView(
-                  children: (doctorData['requests'] as List)
-                      .map((request) => RequestsPage(
-                            doctor: doctorData,
-                            //patient: request,
-                          ))
-                      .toList(),
-                ),
-              ),
-            ],
-          );
-        },
-      ),
-    );
-  }
-}*/
 class DoctorHome extends StatelessWidget {
   const DoctorHome({super.key});
 
@@ -165,8 +20,6 @@ const DoctorView({super.key});
 
 @override
 Widget build(BuildContext context) {
-  //final authModel = Provider.of<AuthModel>(context);
- // Map<String, dynamic> doctor={};
   final textTheme = Theme.of(context).textTheme;
   final colorScheme = Theme.of(context).colorScheme;
 
@@ -235,7 +88,6 @@ class _MyRequests extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-    final authModel = Provider.of<AuthModel>(context);
     final ColorScheme colorScheme = Theme.of(context).colorScheme;
     final TextTheme textTheme = Theme.of(context).textTheme;
 
@@ -345,7 +197,6 @@ return Column(
      
      @override
      Widget build(BuildContext context) {
-       final authModel = Provider.of<AuthModel>(context);
        final ColorScheme colorScheme = Theme.of(context).colorScheme;
        final TextTheme textTheme = Theme.of(context).textTheme;
 
