e246dc7c3dcef16e6779cbd3bb2770e84115becc
torieeee <victoria.mwaura@strathmore.edu>
2024-07-15 10:34:23 +0300

    confirmation

--- laravel_bookpoint_backend/app/Actions/Fortify/PasswordValidationRules.php
+++ laravel_bookpoint_backend/app/Actions/Fortify/PasswordValidationRules.php
@@ -9,6 +9,7 @@ trait PasswordValidationRules
     /**
      * Get the validation rules used to validate passwords.
      * The password must be a string and must be confirmed.
+     * This will require the user to enter the password twice to ensure they have entered it correctly.
      *
      * @return array<int, \Illuminate\Contracts\Validation\Rule|array<mixed>|string>
      */
